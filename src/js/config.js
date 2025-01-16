461a5419f7e6d5130a3cf5e3d4c30de9a4a230c9
Marianna Honcharuk <148065963+MariannaHon@users.noreply.github.com>
2025-01-16 09:12:19 +0100

    finished

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '958ed826-592d-46d8-84d7-d94c779b721e';
+export const MODAL_CLOSE_SEC = 2.5;
